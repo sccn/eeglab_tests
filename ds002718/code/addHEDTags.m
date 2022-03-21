01bebdd75bac224e4469f310a9885cacd5eae2b5
Dung Truong <dt.young112@gmail.com>
2022-03-20 19:09:29 -0700

    attempt to track the rest of ds002718 with git-lfs

--- ds002718/code/addHEDTags.m
+++ ds002718/code/addHEDTags.m
@@ -1,24 +1,3 @@
-%% Script to add HED tags to the datsets
-%% Author: Dung Truong
-
-% path to data folder
-path2data = '/data/projects/BIDSfiles/wakeman-henson-data/ds117forbids';
-% path to field map that map event fields to their prepared HED tags (see
-% wakeman-henson-tags.tsv for a human-readable version
-fMap = '/data/projects/BIDSfiles/wakeman-henson-data/matlab/wh_eeglabcodes/fMap.mat';
-
-if ~exist('pop_tageeg','file'), error('You must install the HEDTools plugin'); end
-
-for isubj=2:19
-    if isubj < 10
-        subjfilename  = ['ds000117_sub00', num2str(isubj), '.set'];
-        foldfername = ['sub00', num2str(isubj)];
-    else
-        subjfilename  = ['ds000117_sub0', num2str(isubj), '.set'];
-        foldfername = ['sub0', num2str(isubj)];
-    end
-    path2set = fullfile(path2data, foldfername,'SET', subjfilename);
-    EEG = pop_loadset('filepath', path2set, 'loadmode', 'info');
-    EEG = pop_tageeg(EEG, 'baseMap', fMap, 'UseGui', false, 'EventFieldsToIgnore', {'face_type','event_type','event_order','time_dist','trial_dist','value'});
-    pop_saveset(EEG, 'filename', subjfilename, 'filepath', fullfile(path2data, foldfername, 'SET'));     
-end
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f2c2b5a59e9ca0959c59b8023d86ee055a0697e83967a0e62700efcd4d4fa95
+size 1145
