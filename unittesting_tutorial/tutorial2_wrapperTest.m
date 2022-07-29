ec9aee9359df7ed26042eabfd2c65560c1e41837
Arnaud Delorme <arnodelorme@gmail.com>
2022-07-29 13:53:30 -0700

    adding simple experiment

--- unittesting_tutorial/tutorial2_wrapperTest.m
+++ unittesting_tutorial/tutorial2_wrapperTest.m
@@ -1,11 +1,11 @@
 function tests = tutorial2_wrapperTest
 % not registered as a test by default
-% tests = functiontests(localfunctions);
+tests = functiontests(localfunctions);
 
 function test_bids_process_face_experiment(~)
-p = fileparts(which('tutorial2_wrapperTest.m'));
-cd(fullfile(p, '..', 'ds002718'));
-bids_process_face_experiment
+%p = fileparts(which('tutorial2_wrapperTest.m'));
+%cd(fullfile(p, '..', 'ds002718'));
+%bids_process_face_experiment
 
 function test_bids_p300(~)
 p = fileparts(which('tutorial2_wrapperTest.m'));
