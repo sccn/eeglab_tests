fc11233af2e1ff86ae7b784587d4a942e6021d12
Arnaud Delorme <arnodelorme@gmail.com>
2023-01-23 23:32:48 -0800

    fix test menu

--- unittesting_adminfunc/eeglab_execmenu/adminfunc_eeglab_execmenu_wrapperTest.m
+++ unittesting_adminfunc/eeglab_execmenu/adminfunc_eeglab_execmenu_wrapperTest.m
@@ -2,4 +2,4 @@ function tests = adminfunc_eeglab_execmenu_wrapperTest
 tests = functiontests(localfunctions);
 
 function test_i_pass_general(~)
-test_eeglab_execmenu;
+evalin('base', 'test_eeglab_execmenu');
